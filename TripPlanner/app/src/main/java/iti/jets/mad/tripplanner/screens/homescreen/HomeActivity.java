cb73ee0c1e7ff0995758e0e7b591da552f6400c3
NouranAmr <nouranamr206@gmail.com>
2019-04-12 17:00:27 +0200

    creation
    creation

---
+++ TripPlanner/app/src/main/java/iti/jets/mad/tripplanner/screens/homescreen/HomeActivity.java
@@ -0,0 +1,15 @@
+package iti.jets.mad.tripplanner.screens.homescreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import iti.jets.mad.tripplanner.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
