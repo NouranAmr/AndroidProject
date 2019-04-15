21fe006ffd36419394d7a566072c4d82510e3080
AhmedAshrafFayeq <47187014+AhmedAshrafFayeq@users.noreply.github.com>
2019-04-15 17:08:34 +0200

    add HomePage

--- TripPlanner/app/src/main/java/iti/jets/mad/tripplanner/screens/homescreen/HomeActivity.java
+++ TripPlanner/app/src/main/java/iti/jets/mad/tripplanner/screens/homescreen/HomeActivity.java
@@ -1,17 +1,21 @@
 package iti.jets.mad.tripplanner.screens.homescreen;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import iti.jets.mad.tripplanner.R;
+import iti.jets.mad.tripplanner.screens.homescreen.historyfragment.HistoryFragment;
+import iti.jets.mad.tripplanner.screens.homescreen.homefragment.HomeFragment;
+import iti.jets.mad.tripplanner.screens.homescreen.profilefragment.ProfileFragment;
 
 public class HomeActivity extends AppCompatActivity {
-    Button btn;
 
     public HomeActivity() {
-
-    //hello world
 
     }
 
@@ -19,15 +23,38 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        btn = findViewById(R.id.button);
-        btn.setOnClickListener((e)->{
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navBottomListener);
 
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+        }
 
-
-
-            
-        });
 
     }
-}
+    private BottomNavigationView.OnNavigationItemSelectedListener navBottomListener=
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment=null;
+                    switch (menuItem.getItemId()){
+
+                        case R.id.nav_home:
+                            selectedFragment=new HomeFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment=new ProfileFragment();
+                            break;
+                        case R.id.nav_history:
+                            selectedFragment=new HistoryFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
+    }
