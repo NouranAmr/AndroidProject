e9a8127553fca75a7d670dab00b61c88aaebbe11
AhmedAshrafFayeq <47187014+AhmedAshrafFayeq@users.noreply.github.com>
2019-04-16 14:13:07 +0200

    add logout

--- TripPlanner/app/src/main/java/iti/jets/mad/tripplanner/screens/homescreen/HomeActivity.java
+++ TripPlanner/app/src/main/java/iti/jets/mad/tripplanner/screens/homescreen/HomeActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
@@ -21,9 +23,20 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar=findViewById(R.id.toolbarID);
+        setSupportActionBar(toolbar);
+
+
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navBottomListener);
 
